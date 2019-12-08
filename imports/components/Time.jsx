592bf04ba11027243e6292cf0aa84396e83a3f5f
sur5ive <karpo.korotko@gmail.com>
2019-12-08 19:41:08 +0200

    test

--- imports/components/Time.jsx
+++ imports/components/Time.jsx
@@ -6,10 +6,12 @@ export default class Time extends React.Component {
       const time = this.props.time;
       const local = new Date();
 
+      console.log(local);
+
       return (
         <div>
           <h3>Current server time: {time}</h3>
-          <h3>Current local time: {local.toTimeString()}</h3>
+          <h3>Current local time: {local.toString()}</h3>
         </div>
       );
     }
