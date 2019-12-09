2506a9c8f5c87a3da51cb2b11a1d1f9bd8faf7fb
sur5ive <karpo.korotko@gmail.com>
2019-12-09 23:36:33 +0200

    .

--- imports/publish/methods.js
+++ imports/publish/methods.js
@@ -1,39 +1,11 @@
 Meteor.methods({
     getServerTime: function () {
         return (new Date).toTimeString();
+    },
+    stopCollection: function () {
+        return null;
+    },
+    startCollection: function () {
+        return null;
     }
 });
-
-/* import { Fixtures } from "../collections";
-
-const CONFIG = require('../config/config');
-
-// Publish all players from the local DB
-Meteor.publish("currentfixture", function () {
-    let self = this;
-    
-    // Get today's date
-    date = new Date();
-    // Subtract 2 hours from the date
-    date.setUTCHours(date.getUTCHours() + Meteor.settings.public.hoursOffset);
-    console.log("Hours offset: " + Meteor.settings.public.hoursOffset);
-
-    let subHandle = Fixtures.find({ts: { $gte : date}}).observeChanges({
-        added: function(id, fields) {
-            self.added("fixtures", id, fields);
-        },
-        changed: function(id, fields) {
-            self.changed("fixtures", id, fields);
-        },
-        removed: function(id) {
-            self.removed("fixtures", id);
-        }
-    });
-
-    self.onStop(function () {
-        subHandle.stop();
-    });
-
-	self.ready();
-});
-*/
