6b1dbac7a38960d60cf3e89b69ae58b141a31dbb
sur5ive <karpo.korotko@gmail.com>
2019-12-15 20:51:50 +0200

    new login

--- server/actions/login.js
+++ server/actions/login.js
@@ -2,12 +2,11 @@ const CREDS = require('../config/creds');
 const CONFIG = require('../config/config');
 
 // DOM element selectors
-const URL_MAIN = 'https://www.fantrax.com/login';
+const URL_MAIN = 'https://www.fantrax.com/fantasy/league/fme67lofjyyvq48x/team/roster';
 const LOGIN_TITLE = 'Fantrax - The Home of Fantasy Sports';
 const SEL_COOKIE= '/html/body/app-root/div/layout-overlay/overlay-toasts/toast/section/div[1]/button[3]';
 const SEL_USERNAME = '//*[@id="mat-input-0"]';
 const SEL_PASSWORD = '//*[@id="mat-input-1"]';
-const SEL_LOGIN_ERROR = "//*[@class='notice notice--error ng-star-inserted']";
 
 export default async function loginPage(page, counter) {
 
@@ -24,49 +23,65 @@ export default async function loginPage(page, counter) {
         await page.waitForXPath(SEL_COOKIE).then((result) => result.click());
         console.log("Cookie popup closed");
 
-        // Enter login details
-        await page.waitForXPath(SEL_USERNAME).then((result) => result.click());
-        await page.keyboard.type(CREDS.username);
-        await page.waitForXPath(SEL_PASSWORD).then((result) => result.click());
-        await page.keyboard.type(CREDS.password);
-        await page.keyboard.press('Enter');
-        console.log("Username/Password entered");
+        await enterLoginDetails(page);
         
-        // Wait for the page to finish loading
-        await page.waitForNavigation().then(async () => {
-            //Check page title of the loaded page
-            await page.title().then(async result => {
-                // Check if logged in page is opened
-                if (result === LOGIN_TITLE) {
-                    // End login flow and return the resulting page object
-                    console.log("Fantrax user logged in");
-                } else {
-                    // Throw error about incorrect page title
-                    throw new Error('Incorrect login page');
-                }
-            });
-        }, async () => {
-            // If wait for navigation fails, check if login error is present
-            await page.waitForXPath(SEL_LOGIN_ERROR).then(async (result) => { 
-                console.log("Login failed: Invalid username/password.");
-                throw new Error('Login failed') 
-            });
-        });
     } catch (e) {
         console.log("Error logging in: " + e);
         // Close the browser and increase counter of tries
-        browser.close();
         counter ++;
 
         // Check if max tries has been reached, otherwise try to login again
         if (counter <= CONFIG.maxLoginTries) {
             console.log("Trying to login again. Try nr: " + counter);
-            await loginPage(counter);
+            await enterLoginDetails(page);
+            
+            //await loginPage(page, counter);
         } else {
             console.log("Maximum number of login tries reached");
             throw new Error('Max login tries reached');
         }
     }
+
+    return page;
+}
+
+async function enterLoginDetails(page) {
+    // Open Fantrax login page
+    await page.goto(URL_MAIN);
+    console.log("Fantrax login page opened");
+    // Enter login details
+    await page.waitForXPath(SEL_USERNAME).then((result) => result.click());
+    await page.keyboard.type(CREDS.username);
+    await page.waitForXPath(SEL_PASSWORD).then((result) => result.click());
+    await page.keyboard.type(CREDS.password);
+    await page.keyboard.press('Enter')
+    console.log("Username/Password entered");
+
+    const cookies = await page.cookies()
+
+    console.log(cookies);
+
+    await Promise.all([
+        page.waitForNavigation(),
+        
+    ]).then(async () => {
+        //Check page title of the loaded page
+        await page.title().then(async result => {
+            console.log("Page finished loading. Title: " + result);
+            // Check if logged in page is opened
+            if (result === LOGIN_TITLE) {
+                // End login flow and return the resulting page object
+                console.log("Fantrax user logged in");
+            } else {
+                // Throw error about incorrect page title
+                throw new Error('Incorrect login page');
+            }
+        });
+    }, async () => {
+        // If wait for navigation fails, check if login error is present
+        console.log("Waiting for response for login page failed");
+        throw new Error('Login failed') 
+    });
 
     return page;
 }
