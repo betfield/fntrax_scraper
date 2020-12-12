74f0c5a99cee33733ed58d8a18447818561f4588
Karpo Korõtko <karpo.korotko@playtech.com>
2020-12-12 10:30:58 +0200

    scoring

---
+++ server/config/fantrax_scring.js
@@ -0,0 +1,79 @@
+export const scoringGK = new Map( 
+    ["AER", 1],
+    ["CLR", 0.25],
+    ["CS", 8],
+    ["GA", -2],
+    ["Int", 1],
+    ["KP", 2],
+    ["PKS", 8],
+    ["RC", -7],
+    ["Sv", 2],
+    ["YC", -3]
+);
+
+export const scoringD = new Map( 
+    ["ACNC", 1],
+    ["AER", 1],
+    ["AT", 8],
+    ["CLR", 0.25],
+    ["CoS", 1],
+    ["CS", 6],
+    ["DIS", -0.5],
+    ["G", 10],
+    ["GAO", -2],
+    ["HT", 10],
+    ["Int", 1],
+    ["KP", 2],
+    ["OG", -5],
+    ["PKM", -4],
+    ["RC", -7],
+    ["SOT", 2],
+    ["SYC", -4],
+    ["TkW", 1],
+    ["YC", -3]
+);
+
+export const scoringM = new Map( 
+    ["ACNC", 1],
+    ["AER", 0.5],
+    ["AT", 6],
+    ["CLR", 0],
+    ["CoS", 1],
+    ["CS", 1],
+    ["DIS", -0.5],
+    ["G", 9],
+    ["GAO", 0],
+    ["HT", 10],
+    ["Int", 1],
+    ["KP", 2],
+    ["OG", -5],
+    ["PKM", -4],
+    ["RC", -7],
+    ["SOT", 2],
+    ["SYC", -4],
+    ["TkW", 1],
+    ["YC", -3]
+);
+
+export const scoringF = new Map( 
+    ["ACNC", 1],
+    ["AER", 0.5],
+    ["AT", 6],
+    ["CLR", 0],
+    ["CoS", 1],
+    ["CS", 0],
+    ["DIS", -0.5],
+    ["G", 9],
+    ["GAO", 0],
+    ["HT", 10],
+    ["Int", 1],
+    ["KP", 2],
+    ["OG", -5],
+    ["PKM", -4],
+    ["RC", -7],
+    ["SOT", 2],
+    ["SYC", -4],
+    ["TkW", 1],
+    ["YC", -3]
+
+);
